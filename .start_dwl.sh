0509f8b0545f614832ed0e3026018bf870bc9803
Michael Vaughan <140607227+micksmakes@users.noreply.github.com>
2026-05-24 15:29:42 +1000

    Add files via upload

--- .start_dwl.sh
+++ .start_dwl.sh
@@ -5,4 +5,4 @@ slstatus -s | dwl -s"sh -c 'swaybg -i /home/mick/wallpaper/city_night.jpg &'"
 swayidle -w \
 timeout 300 'swaylock -f -c 000000' \
 timeout 600 'wlopm --off \*' \
-wlopm --on\*' &
+resume 'wlopm --on\*' &
