e612636670962daf5fd06f8cec1713e0db12a32d
Michael Vaughan <140607227+micksmakes@users.noreply.github.com>
2026-05-24 15:09:14 +1000

    Add files via upload

---
+++ .start_dwl.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+slstatus -s | dwl -s"sh -c 'swaybg -i /home/mick/wallpaper/city_night.jpg &'"
+
+# Idle and screen lock settings
+swayidle -w \
+timeout 300 'swaylock -f -c 000000' \
+timeout 600 'wlopm --off \*' \
+wlopm --on\*' &
